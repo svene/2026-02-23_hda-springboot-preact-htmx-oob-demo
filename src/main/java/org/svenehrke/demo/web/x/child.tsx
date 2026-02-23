e6e06c3e00d28ecfe85303bd3978c16e8ec52fda
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-02-23 20:28:25 +0100

    cleanup

--- src/main/java/org/svenehrke/demo/web/x/child.tsx
+++
@@ -1,9 +0,0 @@
-import { render, h } from "preact";
-
-function Child(props: {message: string}) {
-	return <div style="border-style: solid; border-width: 1px; padding: 5px">
-		<p>Child: {props.message}</p>
-	</div>
-}
-
-export {render, h, Child}
--- src/main/java/org/svenehrke/demo/web/x/hello.tsx
+++
@@ -1,7 +0,0 @@
-import { render, h } from "preact";
-
-function App(props: {message: string}) {
-	return <p>Hello {props.message}</p>;
-}
-
-export {render, h, App}
