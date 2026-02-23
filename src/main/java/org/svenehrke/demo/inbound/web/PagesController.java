4b9d5bf1feecd42b8981cb960dc1096aaf35ca57
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-02-23 14:15:55 +0100

    integrated viewmodels

--- src/main/java/org/svenehrke/demo/inbound/web/PagesController.java
+++ src/main/java/org/svenehrke/demo/inbound/web/PagesController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import tools.jackson.databind.json.JsonMapper;
 
+import java.util.List;
+
 @Controller
 public class PagesController {
 
@@ -27,10 +29,22 @@ public class PagesController {
 	public String page1(Model model) {
 		model.addAttribute("devMode", activeProfile.contains("dev"));
 		model.addAttribute("greetee", "You");
-		record JJJ(String message){};
+		record JJJ(String message) {
+		}
+		;
 		model.addAttribute("jjj", jsonMapper.writeValueAsString(
 			new JJJ("myJJJ")
 		));
+		OOBPersonPageModel vm = new OOBPersonPageModel(
+			new OOBPersonTableModel(
+				List.of(
+					new OOBPersonTableRowModel(1, "Sven", "Ehrke", "street", "detailsLink")
+				),
+				20
+			),
+			"tableLink"
+		);
+		model.addAttribute("vm", vm);
 		return "pages/page1";
 	}
 
---
+++ src/main/java/org/svenehrke/demo/inbound/web/vm/oob-person-page-model-vm.ts
@@ -0,0 +1,43 @@
+export type OOBPersonTableRowModel = {
+	id: number,
+	firstName: string,
+	lastName: string,
+	streetName: string,
+	_detailsLink: string,
+}
+
+export type OOBPersonTableModel = {
+	people: OOBPersonTableRowModel[],
+	total: number,
+}
+export type OOBPersonPageModel = {
+	table: OOBPersonTableModel,
+	_tableLink: string,
+}
+
+export type OOBPersonDetailModel = {
+	id: number,
+	firstName: string,
+	lastName: string,
+	streetName: string,
+	streetNo: string,
+	zipCode: string,
+	city: string,
+	country: string,
+	mailBox: string,
+	phoneNumber: string,
+	cellPhone: string,
+	_backLink: string,
+	_editLink: string,
+	_rowUrl: string,
+}
+
+export type OOBPersonEditModel = {
+	id: number,
+	firstName: string,
+	lastName: string,
+	streetName: string,
+	_editBackLink: string,
+	_submitLink: string,
+}
+
